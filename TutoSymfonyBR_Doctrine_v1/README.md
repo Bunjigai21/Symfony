b5be4ee64ab31e36a8973be545c22729bc425ac9
Bunjigai21 <romain.nicoli83@gmail.com>
2017-01-05 11:50:15 +0100

    avancement tp doctrine

---
+++ TutoSymfonyBR_Doctrine_v1/README.md
@@ -0,0 +1,4 @@
+TutoSymfonyBR_Doctrine_v1
+=========================
+
+A Symfony project created on January 5, 2017, 11:16 am.
---
+++ TutoSymfonyBR_Doctrine_v1/src/AppBundle/Entity/Employe.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Employe
+ *
+ * @ORM\Table(name="employe")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EmployeRepository")
+ */
+class Employe
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salaire", type="decimal", precision=10, scale=0)
+     */
+    private $salaire;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=40, option={"default"="Toulon")
+     */
+    private $ville;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Employe
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Employe
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getville()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Employe
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set salaire
+     *
+     * @param string $salaire
+     *
+     * @return Employe
+     */
+    public function setSalaire($salaire)
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
+
+    /**
+     * Get salaire
+     *
+     * @return string
+     */
+    public function getSalaire()
+    {
+        return $this->salaire;
+    }
+}
+
